a211dff27019a52c211a5207bb87acb4887931bd
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2022-11-19 14:22:47 -0500

    add click to build cli, make ImageProcessor more abstract

--- image_process.py
+++ image_process.py
@@ -1,5 +1,6 @@
 # standard lib
 import logging
+from abc import ABC, abstractmethod
 import os
 from concurrent import futures
 import shutil
@@ -12,7 +13,7 @@ import rasterio
 from rasterio.windows import Window
 
 
-class ImageProcessor:
+class ImageProcessor(ABC):
     IMG_SHAPE_W = 10980
     IMG_SHAPE_H = 10980
 
@@ -26,27 +27,92 @@ class ImageProcessor:
         self.green_band_path = green_band_path
         self.blue_band_path = blue_band_path
 
+    @abstractmethod
+    def process(self) -> Dict[str, np.ndarray]:
+        raise NotImplemented()
+
     @staticmethod
     def num_files_in_dir(path: str):
         _, _, files = next(os.walk(path))
         return len(files)
 
-    def compute_rgb_median(self) -> np.array:
+    @staticmethod
+    def get_profile(path) -> Dict:
+        with rasterio.open(path) as src:
+            profile = src.profile
+        return profile
+
+    def create_composite(self, arr_map: Dict[str, np.ndarray]):
+        # Get geo metadata
+        files = os.listdir(self.red_band_path)
+        meta = self.get_profile(f'{self.red_band_path}{files[0]}')
+        meta.update(count=3)
+        meta['driver'] = 'GTiff'
+
+        dest = f'{self.dest_path}final.tiff'
+        shutil.rmtree(self.dest_path, ignore_errors=True)
+        os.makedirs(self.dest_path)
+        # Read each layer from B->G->R and write it to stack
+        with rasterio.open(dest, 'w', **meta) as dst:
+            dst.write_band(1, arr_map['red'])
+            dst.write_band(2, arr_map['green'])
+            dst.write_band(3, arr_map['blue'])
+
+
+class ArrayMerger(ABC):
+    @abstractmethod
+    def merge(self, arr: np.ndarray) -> np.ndarray:
+        """
+        :param arr: a 3d array where the depth arr[0], arr[1], arr[n] holds multiple versions of the same image or
+        part of an image (2d array). Ex:
+
+        x = [
+            [[1,1,1]
+            ,[1,1,1]]
+        ],
+        [
+            [[3,3,3]
+            ,[3,3,3]]
+        ]
+
+        :return: a 2d array where some computation is performed on each internal 2d array to decide on the final version
+        Ex:
+        mean(x) = [[2,2,2]
+                  [2,2,2]]
+
+        """
         raise NotImplemented
 
-    def create_composite(self, arr: np.array):
-        raise NotImplemented
+
+class MedianMerger(ArrayMerger):
+    def merge(self, arr: np.ndarray) -> np.ndarray:
+        filtered_zeros = np.where(arr == 0.0, np.nan, arr)
+        np.nanmedian(filtered_zeros, axis=0)
+        return np.nanmedian(filtered_zeros, axis=0)
 
 
 class WindowImageProcessor(ImageProcessor):
 
-    def __init__(self, window_size_row=2000, **kwargs):
+    def __init__(self, merger: ArrayMerger, window_size_row=2000, **kwargs):
         super().__init__(**kwargs)
+        self.merger = merger
         self.window_size_row = window_size_row
         self.window_size_column = (0, self.IMG_SHAPE_H)
 
-    def compute_median(self, band: str, path: str) -> np.array:
+    def process(self) -> Dict[str, np.array]:
+        with futures.ProcessPoolExecutor(max_workers=3) as executor:
+            future_red = executor.submit(self.window, 'red', f'{self.red_band_path}')
+            future_green = executor.submit(self.window, 'green', f'{self.green_band_path}')
+            future_blue = executor.submit(self.window, 'blue', f'{self.blue_band_path}')
 
+        executor.shutdown()
+        return {
+            'red': future_red.result()
+            , 'green': future_green.result()
+            , 'blue': future_blue.result()
+        }
+
+    def window(self, band: str, path: str) -> np.ndarray:
         num_of_files = self.num_files_in_dir(path)
         output_arr = np.zeros((self.IMG_SHAPE_W, self.IMG_SHAPE_H))
 
@@ -59,59 +125,22 @@ class WindowImageProcessor(ImageProcessor):
 
             # Are we at the last iteration?
             if (self.IMG_SHAPE_W - row_idx) > self.window_size_row:
-                combined_rows_arr = np.zeros((num_of_files, self.window_size_row, self.IMG_SHAPE_H))
+                multiple_versions_arr = np.zeros((num_of_files, self.window_size_row, self.IMG_SHAPE_H))
             else:
-                combined_rows_arr = np.zeros((num_of_files, self.IMG_SHAPE_W - row_idx, self.IMG_SHAPE_H))
+                multiple_versions_arr = np.zeros((num_of_files, self.IMG_SHAPE_W - row_idx, self.IMG_SHAPE_H))
 
-            # Store are windows for each in file in combined_rows_arr
+            # Store are windows for each in file in multiple_versions_arr
             for i, file in enumerate(os.listdir(path)):
                 with rasterio.open(f'{path}{file}') as src:
                     arr = src.read(1, window=Window.from_slices(
                         (row_idx, row_idx + self.window_size_row), self.window_size_column))
-                    combined_rows_arr[i] = arr
+                    multiple_versions_arr[i] = arr
 
-            #Compute median and assign value to output array
-            filtered_zeros = np.where(combined_rows_arr == 0.0, np.nan, combined_rows_arr)
-            np.nanmedian(filtered_zeros, axis=0)
-            output_arr[row_idx: row_idx + self.window_size_row, :] = np.nanmedian(filtered_zeros, axis=0)
-
-        return output_arr
-
-    def compute_rgb_median(self) -> np.array:
-        with futures.ProcessPoolExecutor(max_workers=3) as executor:
-            future_red = executor.submit(self.compute_median, 'red', f'{self.red_band_path}')
-            future_green = executor.submit(self.compute_median, 'green', f'{self.green_band_path}')
-            future_blue = executor.submit(self.compute_median, 'blue', f'{self.blue_band_path}')
-
-        output_arr = np.zeros((3, self.IMG_SHAPE_W, self.IMG_SHAPE_H))
-        # Order matters here!
-        output_arr[0] = future_blue.result()
-        output_arr[1] = future_green.result()
-        output_arr[2] = future_red.result()
-        executor.shutdown()
+            # Process window
+            out = self.merger.merge(multiple_versions_arr)
+            output_arr[row_idx: row_idx + self.window_size_row, :] = out
 
         return output_arr
-
-    def get_profile(self, path) -> Dict:
-        with rasterio.open(path) as src:
-            profile = src.profile
-        return profile
-
-    def create_composite(self, arr: np.array):
-        # Get the metadata
-        files = os.listdir(self.red_band_path)
-        meta = self.get_profile(f'{self.red_band_path}{files[0]}')
-        meta.update(count=3)
-        meta['driver'] = 'GTiff'
-
-        dest = f'{self.dest_path}stacked-2.tiff'
-        shutil.rmtree(self.dest_path, ignore_errors=True)
-        os.makedirs(self.dest_path)
-        # Read each layer from B->G->R and write it to stack
-        with rasterio.open(dest, 'w', **meta) as dst:
-            dst.write_band(1, arr[0])
-            dst.write_band(2, arr[1])
-            dst.write_band(3, arr[2])
 
 
 class BinaryImageProcessor(ImageProcessor):
--- s2_mosaicker.py
+++ s2_mosaicker.py
@@ -3,34 +3,42 @@ import json
 import logging
 
 # 3rd party
+import click
+
 # lib
 from puller import S3Puller
-from image_process import WindowImageProcessor
+from image_process import WindowImageProcessor, MedianMerger
 
-if __name__ == '__main__':
 
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
-    # Get creds
-    f = open('./keys/aws.json')
-    creds = json.load(f)
-    tile_id = '9DVA'
-    start = '2020-05-09T02:44:33.000000Z'
-    end = '2020-12-26T02:44:33.000000Z'
+@click.command()
+@click.argument('TILE_ID', default='18TTK')
+@click.argument('START_DATETIME', default='2020-05-09T02:44:33.000000Z')
+@click.argument('END_DATETIME', default='2020-12-26T02:44:33.000000Z')
+@click.argument('OUTPUT_PATH', default='./tmp/final/')
+@click.option('--AWS_PATH', default='./keys/aws.json/')
+@click.option('--LOGGING_LEVEL', default='INFO')
+@click.option('--has_pulled', default=True, is_flag=True)
+def main(tile_id, start_datetime, end_datetime, output_path, aws_path, logging_level, has_pulled: bool):
 
-    #Find and filter data
-    puller = S3Puller(config=creds, tile_id=tile_id, start=start, end=end)
-    puller.connect()
-    success = puller.pull_images()
-    if success != 0:
-        logging.fatal('failed to pull images...')
+    logging.basicConfig(level=logging.getLevelName(logging_level), format='%(message)s')
+
+    if not has_pulled:
+        f = open(aws_path)
+        config = json.load(f)
+
+        # Find and filter data
+        puller = S3Puller(config=config, tile_id=tile_id, start=start_datetime, end=end_datetime)
+        puller.connect()
+        success = puller.pull_images()
+        if success != 0:
+            logging.fatal('failed to pull images...')
 
     # Manipulate data
-    process = WindowImageProcessor()
-    arr = process.compute_rgb_median()
-    process.create_composite(arr)
+
+    process = WindowImageProcessor(merger=MedianMerger, window_size_row=1000, dest_path=output_path)
+    final_imgs = process.process()
+    process.create_composite(final_imgs)
 
 
-
-
-
-
+if __name__ == '__main__':
+    main()
