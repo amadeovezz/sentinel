a97be8cae2c524fb0354653edaca643b63f781fd
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2022-11-20 15:21:10 -0500

    improve tests

--- tests/test_puller.py
+++ tests/test_puller.py
@@ -1,144 +1,104 @@
 # standard lib
+from typing import Dict, Tuple
 from datetime import datetime
 from dateutil.tz import tzutc
 
 # 3rd party
 import pytest
-import numpy as np
 
 # lib
 from puller import S3Puller
 
 
+def create_s3_response(
+                         year_month_day: Tuple[int, int, int]
+                       , filename: str, id: int
+                        , tile_id: Tuple = (8,'DV','A')
+                       ) -> Dict:
+
+    year, month, day = year_month_day[0], year_month_day[1], year_month_day[2]
+    filepath = f'tiles/{tile_id[0]}/{tile_id[1]}/{tile_id[2]}/{year}/{month}/{day}/0/{filename}'
+    datetime_obj = datetime(year, month, day, 4, 12, 22, tzinfo=tzutc())
+    return {
+        'Key': filepath
+        , 'LastModified': datetime_obj
+        , 'id': id
+    }
+
+
 class TestS3Puller:
 
-    def test_parse_tile_id(self):
+    def test_single_char_UTM_parse(self):
         tile_id = "8DVA"
-        puller = S3Puller(tile_id=tile_id)
+        start = '2017-08-26T02:44:33.000000Z'
+        end = '2020-08-26T02:44:33.000000Z'
+        puller = S3Puller(tile_id=tile_id, start=start, end=end)
 
         parsed = puller.parse_tile_id()
         assert parsed[0] == '8'
         assert parsed[1] == 'D'
         assert parsed[2] == 'VA'
 
+    def test_multi_char_UTM_parse(self):
         tile_id = "60DVA"
-        puller = S3Puller(tile_id=tile_id)
+        start = '2017-08-26T02:44:33.000000Z'
+        end = '2020-08-26T02:44:33.000000Z'
+        puller = S3Puller(tile_id=tile_id, start=start, end=end)
 
         parsed = puller.parse_tile_id()
         assert parsed[0] == '60'
         assert parsed[1] == 'D'
         assert parsed[2] == 'VA'
 
-
-    def test_filter_s3_dates(self):
+    def test_filter_date_range(self):
         s3_response = [
-            {'Key': 'tiles/9/D/VA/2016/10/19/0/B02.jp2'
-                , 'LastModified': datetime(2016, 10, 19, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 1}
-            , {'Key': 'tiles/9/D/VA/2018/9/1/0/B02.jp2'
-                ,'LastModified': datetime(2018, 9, 1, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 2}
-
-            , {'Key': 'tiles/9/D/VA/2019/11/3/0/B02.jp2'
-                ,'LastModified': datetime(2019, 11, 3, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 3}
-
-            , {'Key': 'tiles/9/D/VA/2019/12/3/0/B03.jp2'
-                ,'LastModified': datetime(2019, 12, 3, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 4}
-
-            , {'Key': 'tiles/9/D/VA/2019/12/3/0/preview/B03.jp2'
-                ,'LastModified': datetime(2019, 12, 3, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 5}
-
-            , {'Key': 'tiles/9/D/VA/2020/2/22/0/B04.jp2'
-                ,'LastModified': datetime(2020, 2, 22, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 6}
-
-            , {'Key': 'tiles/9/D/VA/2021/4/19/0/B01.jp2',
-               'LastModified': datetime(2021, 4, 19, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 7}
+            create_s3_response((2016, 10, 19), 'B02.jp2', 1)
+            , create_s3_response((2017, 1, 2), 'B02.jp2', 2)
+            , create_s3_response((2018, 3, 11), 'B02.jp2', 3)
+            , create_s3_response((2019, 2, 19), 'B02.jp2', 4)
+            , create_s3_response((2020, 6, 7), 'B02.jp2', 5)
+            , create_s3_response((2024, 10, 12), 'B02.jp2', 6)
         ]
 
-        start = '2019-08-26T02:44:33.000000Z'
-        end = '2021-08-26T02:44:33.000000Z'
+        start = '2017-08-26T02:44:33.000000Z'
+        end = '2020-08-26T02:44:33.000000Z'
         puller = S3Puller(tile_id="8DVA", start=start, end=end)
-        filtered_list = puller.filter_s3_files(s3_response, band='red')
-        assert filtered_list[0]['id'] == 6
+        filtered_list = puller.filter_s3_files(s3_response, band='blue')
+        assert filtered_list[0]['id'] == 3
+        assert filtered_list[1]['id'] == 4
+        assert filtered_list[2]['id'] == 5
 
-
-def test_filter_s3_dates(self):
-    s3_response = [
-        {'Key': 'tiles/9/D/VA/2016/10/19/0/B02.jp2'
-            , 'LastModified': datetime(2016, 10, 19, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 1}
-        , {'Key': 'tiles/9/D/VA/2018/9/1/0/B02.jp2'
-            ,'LastModified': datetime(2018, 9, 1, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 2}
-
-        , {'Key': 'tiles/9/D/VA/2019/11/3/0/B02.jp2'
-            ,'LastModified': datetime(2019, 11, 3, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 3}
-
-        , {'Key': 'tiles/9/D/VA/2019/12/3/0/B03.jp2'
-            ,'LastModified': datetime(2019, 12, 3, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 4}
-
-        , {'Key': 'tiles/9/D/VA/2019/12/3/0/preview/B03.jp2'
-            ,'LastModified': datetime(2019, 12, 3, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 5}
-
-        , {'Key': 'tiles/9/D/VA/2020/2/22/0/B04.jp2'
-            ,'LastModified': datetime(2020, 2, 22, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 6}
-
-        , {'Key': 'tiles/9/D/VA/2021/4/19/0/B01.jp2',
-           'LastModified': datetime(2021, 4, 19, 19, 48, 19, tzinfo=tzutc())
-            , 'id': 7}
-    ]
-
-    start = '2019-08-26T02:44:33.000000Z'
-    end = '2021-08-26T02:44:33.000000Z'
-    puller = S3Puller(tile_id="8DVA", start=start, end=end)
-    filtered_list = puller.filter_s3_files(s3_response, band='red')
-    assert filtered_list[0]['id'] == 6
-
-
-
-
-
-def test_filter_s3_response(self):
+    def test_filter_preview_files(self):
         s3_response = [
-            {'Key': 'tiles/9/D/VA/2016/10/19/0/B02.jp2'
-                , 'LastModified': datetime(2016, 10, 19, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 1}
-            , {'Key': 'tiles/9/D/VA/2018/9/1/0/B02.jp2'
-                ,'LastModified': datetime(2018, 9, 1, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 2}
-
-            , {'Key': 'tiles/9/D/VA/2019/11/3/0/B10.jp2'
-                ,'LastModified': datetime(2019, 11, 3, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 3}
-
-            , {'Key': 'tiles/9/D/VA/2019/12/3/0/B03.jp2'
-                ,'LastModified': datetime(2019, 12, 3, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 4}
-
-            , {'Key': 'tiles/9/D/VA/2019/12/3/0/preview/B03.jp2'
-                ,'LastModified': datetime(2019, 12, 3, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 5}
-
-            , {'Key': 'tiles/9/D/VA/2020/2/22/0/B04.jp2'
-                ,'LastModified': datetime(2020, 2, 22, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 6}
-
-            , {'Key': 'tiles/9/D/VA/2021/4/19/0/B01.jp2',
-               'LastModified': datetime(2021, 4, 19, 19, 48, 19, tzinfo=tzutc())
-                , 'id': 7}
+            create_s3_response((2016, 10, 19), 'preview/B02.jp2', 1)
+            , create_s3_response((2017, 1, 2), 'B02.jp2', 2)
+            , create_s3_response((2018, 3, 11), 'preview/B02.jp2', 3)
+            , create_s3_response((2019, 2, 19), 'B02.jp2', 4)
+            , create_s3_response((2020, 6, 7), 'B02.jp2', 5)
         ]
 
-        start = '2019-08-26T02:44:33.000000Z'
+        start = '2015-08-26T02:44:33.000000Z'
         end = '2021-08-26T02:44:33.000000Z'
         puller = S3Puller(tile_id="8DVA", start=start, end=end)
-        filtered_list = puller.filter_s3_files(s3_response, band='red')
-        assert filtered_list[0]['id'] == 6
+        filtered_list = puller.filter_s3_files(s3_response, band='blue')
+        assert filtered_list[0]['id'] == 2
+        assert filtered_list[1]['id'] == 4
+        assert filtered_list[2]['id'] == 5
+
+    def test_filter_band(self):
+        s3_response = [
+            create_s3_response((2016, 10, 19), 'B02.jp2', 1)
+            , create_s3_response((2017, 1, 2), 'B04.jp2', 2)
+            , create_s3_response((2018, 3, 11), 'B03.jp2', 3)
+            , create_s3_response((2019, 2, 19), 'B10.jp2', 4)
+            , create_s3_response((2020, 6, 7), 'B02.jp2', 5)
+            , create_s3_response((2024, 10, 12), 'B02.jp2', 6)
+        ]
+
+        start = '2013-08-26T02:44:33.000000Z'
+        end = '2030-08-26T02:44:33.000000Z'
+        puller = S3Puller(tile_id="8DVA", start=start, end=end)
+        filtered_list = puller.filter_s3_files(s3_response, band='blue')
+        assert filtered_list[0]['id'] == 1
+        assert filtered_list[1]['id'] == 5
+        assert filtered_list[2]['id'] == 6
